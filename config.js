bb32607373503a3f863b96548b3499d869b49653
IK <ik@1.pool85-62-99.static.orange.es>
2019-06-22 12:23:48 +0200

    config

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
 module.exports = {
     'secretKey': 'somesecretkey',
-    'mongoUrl': 'mongodb://ilyak:Mongo!1738@mad1r-ntwvh.mongodb.net:27017/mad1r?retryWrites=true&w=majority'
+    'mongoUrl': 'mongodb+srv://mulder:Henk%3D2000@cluster0-633i2.mongodb.net/test?retryWrites=true&w=majority',
     'subscriptionKey': 'somesubscriptionkey',
-    'url': 'https://mad1r.herokuapp.com'
+    'url': 'mad1r.herokuapp.com'
 }
 
 // 'mongoUrl': 'mongodb+srv://ilyak:Mongo!738@mad1r-ntwvh.mongodb.net/test?retryWrites=true&w=majority',
+//     'mongoUrl': 'mongodb+srv://mulder:Henk%3D2000@cluster0-633i2.mongodb.net/test?retryWrites=true&w=majority',
+// mongodb+srv://ilyak:<password>@mad1r-ntwvh.mongodb.net/test?retryWrites=true&w=majority
+
+// 'mongoUrl': 'mongodb+srv://ilyak:Henk%3D2000@mad1r-ntwvh.mongodb.net/mad1r?retryWrites=true&w=majority',
