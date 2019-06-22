5dafb098cfad6ce0dd8594c6d46eeddc03a29930
IK <ik@1.pool85-62-99.static.orange.es>
2019-06-22 11:43:46 +0200

    config again

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
     'secretKey': 'somesecretkey',
-    'mongoUrl': 'mongodb+srv://ilyak:Mongo!738@mad1r-ntwvh.mongodb.net/test?retryWrites=true&w=majority',
+    'mongoUrl': 'ilyak:Mongo!1738@mad1r-shard-00-01-ntwvh.mongodb.net.:27017/mad1r.mad1r_collection',
     'subscriptionKey': 'somesubscriptionkey',
     'url': 'https://mad1r.herokuapp.com'
 }
+
+// 'mongoUrl': 'mongodb+srv://ilyak:Mongo!738@mad1r-ntwvh.mongodb.net/test?retryWrites=true&w=majority',
