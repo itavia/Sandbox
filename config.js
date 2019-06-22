3e3cf4ab47e352dd665d83f6bc2ac368696d7c93
IK <ik@1.pool85-62-99.static.orange.es>
2019-06-22 12:49:35 +0200

    config

--- config.js
+++ config.js
@@ -1,9 +1,18 @@
 module.exports = {
-    'secretKey': 'somesecretkey',
-    'mongoUrl': 'mongodb+srv://mulder:Henk%3D2000@cluster0-633i2.mongodb.net/test?retryWrites=true&w=majority',
-    'subscriptionKey': 'somesubscriptionkey',
+    'secretKey': 'mysecretkey',
+    'mongoUrl': 'mongodb+srv://andrey:Office%21%408@onerecord-0xi21.mongodb.net/test?retryWrites=true&w=majority',
+    'subscriptionKey': 'subscriptionKey',
     'url': 'mad1r.herokuapp.com'
 }
+
+
+
+// {
+//     'secretKey': 'somesecretkey',
+//     'mongoUrl': 'mongodb+srv://mulder:Henk%3D2000@cluster0-633i2.mongodb.net/test?retryWrites=true&w=majority',
+//     'subscriptionKey': 'somesubscriptionkey',
+//     'url': 'mad1r.herokuapp.com'
+// }
 
 // 'mongoUrl': 'mongodb+srv://ilyak:Mongo!738@mad1r-ntwvh.mongodb.net/test?retryWrites=true&w=majority',
 //     'mongoUrl': 'mongodb+srv://mulder:Henk%3D2000@cluster0-633i2.mongodb.net/test?retryWrites=true&w=majority',
