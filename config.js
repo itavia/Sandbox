e6aabd68ad312e8165fc57dc415e47f99ac33cba
Ilya <8775119+ikuznetsow@users.noreply.github.com>
2019-06-22 14:17:16 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,6 @@
 module.exports = {
     'secretKey': 'mysecretkey',
-    'mongoUrl': 'mongodb+srv://andrey:Office%21%408@onerecord-0xi21.mongodb.net/test?retryWrites=true&w=majority',
+    'mongoUrl': '',
     'subscriptionKey': 'subscriptionKey',
-    'url': 'mad1r.herokuapp.com'
+    'url': ''
 }
-
-
-
-// {
-//     'secretKey': 'somesecretkey',
-//     'mongoUrl': 'mongodb+srv://mulder:Henk%3D2000@cluster0-633i2.mongodb.net/test?retryWrites=true&w=majority',
-//     'subscriptionKey': 'somesubscriptionkey',
-//     'url': 'mad1r.herokuapp.com'
-// }
-
-// 'mongoUrl': 'mongodb+srv://ilyak:Mongo!738@mad1r-ntwvh.mongodb.net/test?retryWrites=true&w=majority',
-//     'mongoUrl': 'mongodb+srv://mulder:Henk%3D2000@cluster0-633i2.mongodb.net/test?retryWrites=true&w=majority',
-// mongodb+srv://ilyak:<password>@mad1r-ntwvh.mongodb.net/test?retryWrites=true&w=majority
-
-// 'mongoUrl': 'mongodb+srv://ilyak:Henk%3D2000@mad1r-ntwvh.mongodb.net/mad1r?retryWrites=true&w=majority',
