8d8d962781e1aabfe3ee1d5a849af90550b60181
IK <ik@1.pool85-62-99.static.orange.es>
2019-06-22 11:55:10 +0200

    config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': 'somesecretkey',
-    'mongoUrl': 'ilyak:Mongo!1738@mad1r-shard-00-00-ntwvh.mongodb.net:27017/mad1r',
+    'mongoUrl': 'mongodb://ilyak:Mongo!1738@mad1r-ntwvh.mongodb.net:27017/mad1r?retryWrites=true&w=majority'
     'subscriptionKey': 'somesubscriptionkey',
     'url': 'https://mad1r.herokuapp.com'
 }
